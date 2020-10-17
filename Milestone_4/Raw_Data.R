19096b41b54a783b5f4b9801ec28979322a00473
Rom Blanco <romblanco7@gmail.com>
2020-10-17 11:09:57 +0800

    Second commit

--- Milestone_4/Raw_Data.R
+++ Milestone_4/Raw_Data.R
@@ -2,35 +2,11 @@ library(readxl)
 library(dplyr)
 library(janitor)
 library(tidyverse)  
-Location_data <- read_excel("Milestone_4/place.pop.precip.war.xlsx", sheet = c(3))
+
+# loading datasets for the final project
+  Location_data <- read_excel("Milestone_4/place.pop.precip.war.xlsx", sheet = c(3))
   Population_data <- read_excel("Milestone_4/place.pop.precip.war.xlsx", sheet = c(4))
   Precipitation_data <- read_excel("Milestone_4/place.pop.precip.war.xlsx", sheet = c(5))
   War_data <- read_excel("Milestone_4/place.pop.precip.war.xlsx", sheet = c(6))
   
   
-  Loc_data_clean <-  Location_data %>%
-     clean_names() %>%
-     select("primary", "population_data_name", "climate_data_name", "war_data_ccode", "war_data_state_name", "latitude", "longitude")
-  
-    
-  
-  
-  navbarPage("Population, Precipitation and War", collapsible = TRUE, inverse = TRUE, theme = shinytheme("cerulean"),
-             
-             #First tab called ABOUT. This page contains the project description and the source of the data used.            
-             tabPanel("ABOUT", 
-                      h3("Project Description"),
-                      h4("This project analyzes and compares data on the population, precipitation and war in 196
-countries from 1800 to 2010. The overall aims of the project is to understand
-the relationship between precipitation and population as well as precipitation
-and war.  Ultimately, goal is to see whether or not there is a strong link
-between precipitation the population as well as between precipitation and
-conflicts. The result ofth is study can influence the way we view our
-environment as a whole and major environmental issues such as global warming."),
-                      
-                      h4("The datasets used in this study come from the Harvard Dataverse, linked
-through the following URL https://dataverse.harvard.edu/dataset.
-xhtml?persistentId=doi:10.7910/DVN/ZN1WLF"),
-                      
-                      #Second lab called Location
-                      tabPanel("Location"))) 
--- Milestone_4/milestone_4.R
+++ Milestone_4/milestone_4.R
@@ -2,17 +2,24 @@ library(shiny)
 library(shinythemes)
 library(readxl)
 library(janitor)
+library(tidyverse)
 
-# Cleaning Location data 
+# Cleaning Location data. From this dataset, we just want columns containing the different names
+# countries are referred to as well as their latitudinal and longitudinal
+# locations.
 
  Loc_data_clean <-  Location_data %>%
     clean_names() %>%
     select("primary", "population_data_name", "climate_data_name", "war_data_ccode", "war_data_state_name", "latitude", "longitude") 
 
+ 
+#Coding shiny app 
 
 ui <- fluidPage(navbarPage("Population, Precipitation and War", collapsible = TRUE, inverse = TRUE, theme = shinytheme("cyborg"),
-                           
-#First tab called ABOUT. This page contains the project description and the source of the data used.            
+         
+                                             
+#First tab called ABOUT. This page contains the project description and the source of the data used. 
+
     tabPanel("ABOUT", 
           h3("Project Description"),
           h4("This project analyzes and compares data on the population, precipitation
@@ -21,45 +28,58 @@ there is a strong link between precipitation the population as well as
 precipitation and world conflicts. The result of this study can influence the way we
 view our environment as a whole and the way we see major environmental issues such as global
 warming."),
-          h4("The datasets used in this study come from the Harvard Dataverse, linked
-through the following URL https://dataverse.harvard.edu/dataset.
-xhtml?persistentId=doi:10.7910/DVN/ZN1WLF")),
-                                    
-#Second tab called LOCATIONs containing the latitudimal and longitudinal locations of 196 countries
+          h4("The datasets used in this study come from the Harvard Dataverse and Oregon State Univeristy, linked
+through the following URLs (1) https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/ZN1WLF; (2)https//prism.oregonstate.edu/historical/"),
+          h5("Repository: https://github.com/romblanco7/Shiny_App_Final_Project.git")),
+   
+                                 
+#Second tab called LOCATIONS containing the latitudinal and longitudinal locations of 196 countries
+
   tabPanel(title= "LOCATIONS", 
-           h4("This page helps us determine the latitudinal and longitudinal locations of the 196 countries.
+           h5("This page helps us determine the latitudinal and longitudinal locations of the 196 countries.
            These can be used in standardizing the locations of precipitation and wars which we will be looking at"),
            
-             sidebarLayout(
-             sidebarPanel(
+#Sidebar layout
+
+           sidebarLayout(
+              
+#Sidebar Panel to select a country    
+              
+                sidebarPanel(
                selectInput("Countries", "Select country:", choices = Loc_data_clean$primary)),
+               
+#Main Panel that plot the location of the country selected  
+
              mainPanel("Country location",
                plotOutput("plot")))),
-#Additional tabs. To be developed.
-   tabPanel(title= "POPULATION", h4("To be developed")),
-   tabPanel(title= "WAR", h4("To be developed")),
-   tabPanel(title= "PRECIPITATION", h4("To be developed")),
-   tabPanel(title = "DISCUSSION", h4("To be developed"))
+
+#Additional tabs. To be developed.   
+
+   tabPanel(title = "PRECIPITATION", 
+            h4("To be developed")),
+   tabPanel(title = "POPULATION", 
+            h4("To be developed")),
+   tabPanel(title = "WAR", 
+            h4("To be developed")),
+   tabPanel(title = "DISCUSSION", 
+            h4("To be developed"))
+
 
 ))
 
 
 server <- function(input, output) {
-  
-  
+
 # Plot showing  the latitudinal and longitudinal location of 196 countries
+   
    output$plot <- renderPlot({
-   ggplot(data = Loc_data_clean[Loc_data_clean$primary==input$Countries,], mapping = aes(x = latitude, y = longitude)) +
+   ggplot(data = Loc_data_clean[Loc_data_clean$primary == input$Countries,], mapping = aes(x = latitude, y = longitude)) +
           geom_point(color="blue", size = 5) +
-     theme_grey()
-     
-   })
+          theme_grey()
+         })
    
-   
-   
+
  }
   
-
-
 
 shinyApp(ui = ui, server = server)
